884dde28b10ff1a2a4a000d1c498436fd24c2e53
Jared Branum <jared.branum@gmail.com>
2012-05-02 01:40:32 -0400

    pull episode titles from tvrage api when renaming

--- tv_renamer.rb
+++ tv_renamer.rb
@@ -1,6 +1,12 @@
 #!/usr/bin/env ruby
+require 'yaml'
+require 'net/http'
+require 'nokogiri'
+yaml_path = File.join(File.expand_path(File.dirname(__FILE__)), 'api.yaml')
+api_yaml = YAML::load(File.open(yaml_path))
+API_PATH = api_yaml['path']
 
-def rename_season(n=1)
+def rename_season(n=1, titles=false)
   show = File.basename(Dir.pwd)
   dir = "#{Dir.pwd}/Season #{n}"
   ep = 0
@@ -12,7 +18,8 @@ def rename_season(n=1)
       ep += 1
     end
     ext = item[/\.([^\.]+)$/,1]
-    new_name = "#{show} - S#{n>9 ? n : '0'+n.to_s}E#{ep>9 ? ep : '0'+ep.to_s}.#{ext}"
+    ep_title = get_title(show, n, ep)
+    new_name = "#{show} - S#{pad_zero(n)}E#{pad_zero(ep)} - #{ep_title}.#{ext}"
     puts "Rename \"#{item}\" to \"#{new_name}\"? [yes/no]"
     yn = gets.chomp
     if /^y(?:(?:es)|$)/i.match(yn)
@@ -28,13 +35,25 @@ def rename_season(n=1)
   end
 end
 
+def get_title(series, season, ep)
+  url = URI.parse(URI.encode(API_PATH + "&show=#{series}&ep=#{season}x#{ep}"))
+  res = Net::HTTP.get_response(uri)
+  doc = Nokogiri::XML(res.body)
+  doc.xpath('/show/episode/title').first.content
+end
+
+def pad_zero(n)
+  n = n.to_i
+  n > 9 ? n.to_s : "0#{n}"
+end
+
 subdirs = Dir.glob("*/").reject{|x| /^Season (\d+)\/$/i.match(x).nil? }
-if subdirs.count > 0 # season subdirs
+if subdirs.count > 0 # season subdirs (multiple seasons)
   subdirs.each do |s|
     next unless (season = /^Season (\d+)\/$/i.match(s)) && season.length > 1
     rename_season(season[1].to_i)
   end
-else # multiple seasons
+else # single season
   if season = /^Season (\d+)$/i.match(File.basename(Dir.pwd))
     Dir.chdir("..")
     rename_season(season[1].to_i)
