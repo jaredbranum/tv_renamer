c335b6cefe8b392b0ccbfa5422ba028d93e1988a
Jared Branum <jared.branum@gmail.com>
2012-05-15 19:45:54 -0400

    added command-line flags for force-title, skip-title, and yes (to all)

--- tv_renamer.rb
+++ tv_renamer.rb
@@ -3,11 +3,37 @@ require 'rubygems'
 require 'yaml'
 require 'net/http'
 require 'nokogiri'
-yaml_path = File.join(File.expand_path(File.dirname(__FILE__)), 'api.yaml')
-api_yaml = YAML::load(File.open(yaml_path))
-API_PATH = api_yaml['path']
 
-def rename_season(n=1, titles=false)
+$settings = {
+  :use_title => true,
+  :force_title => false,
+  :ask_every_time => true,
+}
+def process_arg(arg)
+  case arg
+  when 'f', 'force-title' then $settings[:force_title] = true
+  when 'n', 'no-title' then $settings[:use_title] = false
+  when 'y', 'yes' then $settings[:ask_every_time] = false
+  end
+end
+ARGV.each do |a|
+  if a.length == 1
+    process_arg(a)
+  elsif a.length > 2 && a[0..1] == '--'
+    process_arg(a[2..-1])
+  elsif a[0] == '-'
+    a[1..-1].each_char{|c| process_arg(c) }
+  end
+end
+$settings[:use_title] = true if $settings[:force_title]
+
+if $settings[:use_title]
+  yaml_path = File.join(File.expand_path(File.dirname(__FILE__)), 'api.yaml')
+  api_yaml = YAML::load(File.open(yaml_path))
+  API_PATH = api_yaml['path']
+end
+
+def rename_season(n=1)
   show = File.basename(Dir.pwd)
   dir = "#{Dir.pwd}/Season #{n}"
   ep = 0
@@ -19,16 +45,29 @@ def rename_season(n=1, titles=false)
       ep += 1
     end
     ext = item[/\.([^\.]+)$/,1]
-    ep_title = get_title(show, n, ep)
+    ep_title = if $settings[:force_title]
+      title = nil
+      while title.nil? do
+        title = get_title(show, n, ep)
+      end
+      title
+    else
+      $settings[:use_title] ? get_title(show, n, ep) : nil
+    end
     new_name = if ep_title
       "#{show} - S#{pad_zero(n)}E#{pad_zero(ep)} - #{ep_title}.#{ext}"
     else
       "#{show} - S#{pad_zero(n)}E#{pad_zero(ep)}.#{ext}"
     end
     next if item == new_name
-    puts "Rename \"#{item}\" to \"#{new_name}\"? [yes/no]"
-    yn = gets.chomp
-    if /^y(?:(?:es)|$)/i.match(yn)
+    if $settings[:ask_every_time]
+      puts "Rename \"#{item}\" to \"#{new_name}\"? [yes/no]"
+      yn = gets.chomp
+      confirm = /^y(?:(?:es)|$)/i.match(yn)
+    else
+      confirm = true
+    end
+    if confirm
       begin
         File.rename("#{dir}/#{item}", "#{dir}/#{new_name}")
         puts "File renamed."
